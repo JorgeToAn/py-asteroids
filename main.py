f7599111c58db836d88cc565bcdaef119c135e3f
JorgeToAn <jorgetosta.f@gmail.com>
2025-09-09 18:07:30 -0700

    chore: created updateables & drawables groups

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updateables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updateables, drawables)
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     # Game loop
@@ -17,8 +22,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color_black)
-        player.update(dt)
-        player.draw(screen)
+        
+        updateables.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(FRAMERATE) / 1000
 
