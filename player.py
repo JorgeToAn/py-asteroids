36e759b1c89cc008da33b4765a9e574791a27317
JorgeToAn <jorgetosta.f@gmail.com>
2025-09-30 16:38:27 -0700

    feat: implement acceleration

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import pygame
 
 from circleshape import CircleShape
 from shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED, PLAYER_SHOOT_COOLDOWN
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_ACCELERATION, PLAYER_SHOT_SPEED, PLAYER_SHOOT_COOLDOWN
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -10,7 +10,6 @@ class Player(CircleShape):
         self.rotation = 0
         self.shoot_cooldown = 0
     
-    # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -22,9 +21,12 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
     
-    def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+    def move(self, direction, dt):
+        if direction:
+            self.velocity += direction * PLAYER_ACCELERATION * dt
+        else:
+            self.velocity -= self.velocity * dt
+        self.position += self.velocity
 
     def draw(self, screen):
         color_white = "#ffffff"
@@ -43,12 +45,18 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
-            self.move(dt)
-        if keys[pygame.K_s]:
-            self.move(-dt)
+
+        input_dir = pygame.Vector2(0, self.get_input_axis(pygame.K_s, pygame.K_w)).rotate(self.rotation)
+        self.move(input_dir, dt)
+
         if keys[pygame.K_SPACE] and self.shoot_cooldown <= 0:
             self.shoot()
 
         self.shoot_cooldown -= dt
     
+    def get_input_axis(self, negative_key, positive_key):
+        keys = pygame.key.get_pressed()
+
+        if keys[negative_key] == keys[positive_key]:
+            return 0
+        return 1 if keys[positive_key] else -1
